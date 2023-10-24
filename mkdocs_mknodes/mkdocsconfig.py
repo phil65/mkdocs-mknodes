ec293aea352750063b9a71a560929569a7b49517
phil65 <philipptemminghoff@googlemail.com>
2023-10-24 10:20:22 +0200

    chore: eat load_config from mkdocs

--- mkdocs_mknodes/mkdocsconfig.py
+++ mkdocs_mknodes/mkdocsconfig.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from collections.abc import Mapping, Sequence
+from collections.abc import Iterator, Mapping, Sequence
 import contextlib
 
 from datetime import datetime
@@ -8,12 +8,12 @@ import functools
 import io
 import os
 import pathlib
-from typing import Any
+import sys
+from typing import IO, Any
 from urllib import parse
 
 import jinja2
 
-from mkdocs import config as _config
 from mkdocs.commands import get_deps
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import get_plugin_logger
@@ -26,11 +26,93 @@ from mknodes.utils import pathhelpers, reprhelpers
 logger = get_plugin_logger(__name__)
 
 
+@contextlib.contextmanager
+def _open_config_file(config_file: str | os.PathLike | IO | None) -> Iterator[IO]:
+    """A context manager which yields an open file descriptor ready to be read.
+
+    Accepts a filename as a string, an open or closed file descriptor, or None.
+    When None, it defaults to `mkdocs.yml` in the CWD. If a closed file descriptor
+    is received, a new file descriptor is opened for the same file.
+
+    The file descriptor is automatically closed when the context manager block is existed.
+    """
+    match config_file:
+        case None:
+            paths_to_try = [pathlib.Path("mkdocs.yml")]
+        case str() | os.PathLike():
+            paths_to_try = [pathlib.Path(config_file)]
+        case _ if getattr(config_file, "closed", False):
+            # If closed file descriptor, get file path to reopen later.
+            paths_to_try = [pathlib.Path(config_file.name)]
+        case _:
+            result_config_file = config_file
+            paths_to_try = None
+
+    if paths_to_try:
+        # config_file is not a file descriptor, so open it as a path.
+        for path in paths_to_try:
+            path = path.resolve()
+            logger.debug("Loading configuration file: %r", path)
+            try:
+                result_config_file = open(path, "rb")  # noqa: SIM115, PTH123
+                break
+            except FileNotFoundError:
+                continue
+        else:
+            msg = f"Config file {paths_to_try[0]!r} does not exist."
+            raise SystemExit(msg)
+    else:
+        logger.debug("Loading configuration file: %r", result_config_file)
+        # Ensure file descriptor is at beginning
+        with contextlib.suppress(OSError):
+            result_config_file.seek(0)
+
+    try:
+        yield result_config_file
+    finally:
+        if hasattr(result_config_file, "close"):
+            result_config_file.close()
+
+
 @functools.cache
-def load_config(path: str | os.PathLike | None = None) -> MkDocsConfig:
-    path = None if path is None else str(path)
-    cfg = _config.load_config(path)
-    logger.info("Loaded config from path '%s'", path or "mkdocs.yml")
+def load_config(
+    config_file: str | IO | None = None,
+    *,
+    config_file_path: str | None = None,
+    **kwargs,
+) -> MkDocsConfig:
+    """Load the configuration for a given file object or name.
+
+    The config_file can either be a file object, string or None. If it is None
+    the default `mkdocs.yml` filename will loaded.
+
+    Extra kwargs are passed to the configuration to replace any default values
+    unless they themselves are None.
+    """
+    options = {k: v for k, v in kwargs.copy().items() if v is not None}
+    with _open_config_file(config_file) as fd:
+        # Initialize the config with the default schema.
+
+        if config_file_path is None and fd is not sys.stdin.buffer:
+            config_file_path = getattr(fd, "name", None)
+        cfg = MkDocsConfig(config_file_path=config_file_path)
+        cfg.load_file(fd)
+
+    # Then load the options to overwrite anything in the config.
+    cfg.load_dict(options)
+
+    errors, warnings = cfg.validate()
+
+    for config_name, warning in warnings + errors:
+        logger.warning("Config value %r: %s", config_name, warning)
+    for k, v in cfg.items():
+        logger.debug("Config value %r = %r", k, v)
+    if len(errors) > 0:
+        msg = f"Aborted with {len(errors)} configuration errors!"
+        raise SystemExit(msg)
+    if cfg.strict and len(warnings) > 0:
+        msg = f"Aborted with {len(warnings)} configuration warnings in 'strict' mode!"
+        raise SystemExit(msg)
     return cfg
 
 
@@ -176,5 +258,5 @@ class Config:
 
 
 if __name__ == "__main__":
-    cfg = Config()
-    print(dict(cfg.theme))
+    test = Config()
+    print(test)
